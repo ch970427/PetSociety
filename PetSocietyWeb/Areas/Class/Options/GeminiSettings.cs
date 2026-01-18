c342207a7056ef6b57ff38e518aa159826f0011e
ch970427 <ch970427@gmail.com>
2026-01-18 22:25:08 +0800

    first commit

---
+++ PetSocietyWeb/Areas/Class/Options/GeminiSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace PetSocietyWeb.Areas.Class.Options
+{
+    public class GeminiSettings
+    {
+        public string ApiKey { get; set; } = string.Empty;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PetSociety
+A web application built with Angular 19 and ASP.NET 9 Core API.
